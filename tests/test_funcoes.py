2289872d7c1710125d55d06b69f2602e81803a15
Fernando Franklim M de Campos <fernando-franklim@hotmail.com>
2025-09-18 19:10:09 -0300

    Create test_funcoes.py

---
+++ tests/test_funcoes.py
@@ -0,0 +1,18 @@
+from funcoes import soma, subtrai, multiplica, divide, eh_par
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+def test_subtrai():
+    assert subtrai(5, 3) == 2
+
+def test_multiplica():
+    assert multiplica(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(10, 0) == "Erro: divisão por zero"
+
+def test_eh_par():
+    assert eh_par(4) is True
+    assert eh_par(5) is False
